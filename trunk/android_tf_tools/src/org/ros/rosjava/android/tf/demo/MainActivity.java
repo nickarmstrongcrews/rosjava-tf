15b66e6559719c6227c4806d23faf5d4b51c89f8
nick@heuristiclabs.com <nick@heuristiclabs.com@489242e3-ed30-23bf-00e1-e622806e17b2>
2011-09-08 03:30:04 +0000

    made RosTfView work; greatly improved demo app (shows new features, less ugly)

--- trunk/android_tf_tools/src/org/ros/rosjava/android/tf/demo/MainActivity.java
+++ trunk/android_tf_tools/src/org/ros/rosjava/android/tf/demo/MainActivity.java
@@ -22,22 +22,13 @@ import android.app.Activity;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 
-import org.ros.message.tf.tfMessage;
 import org.ros.node.DefaultNodeFactory;
 import org.ros.node.Node;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeRunner;
-import org.ros.rosjava.android.MessageCallable;
-import org.ros.rosjava.android.OrientationPublisher;
-import org.ros.rosjava.android.views.RosTextView;
 import org.ros.rosjava.android.tf.android_tf_tools.OrientationPublisherTf;
 import org.ros.rosjava.android.tf.android_tf_tools.R;
-import org.ros.rosjava.android.tf.android_tf_tools.R.id;
-import org.ros.rosjava.android.tf.android_tf_tools.R.layout;
-import org.ros.rosjava.tf.StampedTransform;
-import org.ros.rosjava.tf.Transform;
-import org.ros.rosjava.tf.TransformTree;
-import org.ros.rosjava.tf.adt.TransformFactory;
+import org.ros.rosjava.android.tf.android_tf_tools.RosTfView;
 import org.ros.rosjava.tf.pubsub.TransformListener;
 
 /**
@@ -47,57 +38,22 @@ public class MainActivity extends Activity {
 
   protected final NodeRunner nodeRunner;
   
-  protected RosTextView<org.ros.message.tf.tfMessage> tfPrinter;
+  protected RosTfView tfPrinter;
   protected OrientationPublisherTf oPubTf;
   
-  protected TransformTree tfTree;
+  //protected final TransformTree tfTree;
   
   public MainActivity() {
 	super();
     nodeRunner = NodeRunner.newDefault();
+    //tfTree = new TransformTree();
   }
   
-  @SuppressWarnings("unchecked")
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    /*
     tfPrinter = (RosTfView) findViewById(R.id.text);
- 	*/
-    tfPrinter = (RosTextView<org.ros.message.tf.tfMessage>) findViewById(R.id.text);
-    tfPrinter.setTopicName("/tf");
-    tfPrinter.setMessageType("tf/tfMessage");
-    tfPrinter
-        .setMessageToStringCallable(new MessageCallable<String, org.ros.message.tf.tfMessage>() {
-          @Override
-          public String call(org.ros.message.tf.tfMessage msg) {
-        	  String s = "";
-        	  //String s = "{ ";
-        	  for( org.ros.message.geometry_msgs.TransformStamped tx :  msg.transforms ) {
-        		  tfTree.add(TransformFactory.fromTfMessage(msg));
-        		  //s += tx.header.stamp + ": " + tx.header.frame_id + "->" + tx.child_frame_id;
-        		  //s += "\n" + TransformFactory.fromTfMessage(msg).size() + "\n";
-        		  //s += tx.toString();
-        		  //Transform tx2 = tfTree.lookupTransformBetween(tx.header.frame_id, tx.child_frame_id, tx.header.stamp.totalNsecs());
-        		  //Transform tx2 = tfTree.lookupTransformBetween(tx.header.frame_id, tx.child_frame_id, tx.header.stamp.totalNsecs() - 200000000l);
-        		  //Transform tx2 = tfTree.lookupTransformBetween("/world", "/robot", tx.header.stamp.totalNsecs() - 200000000l);
-//        		  if(tfTree.canTransform("/world", "/odom", tx.header.stamp.totalNsecs())) s += "yes!\n";
-//        		  else s += "no!\n";        		  
-        		  //if(tfTree.canTransform("/world", "/robot")) s += "yes!\n";
-        		  //else s += "no!\n";
-        	  }
-    		  if(tfTree.canTransform("/world", "/phone_oriented")) {
-    			  Transform tx2 = tfTree.lookupMostRecent("/world", "/phone_oriented");
-    			  s += tx2.toString();
-//    			  s += "OK!\n";
-    		  } else {
-    			  s += "not OK :(\n";
-    		  }
-        	  //s += "\n}";
-            return s;
-          }
-        });
   }
   
   @Override
@@ -112,16 +68,12 @@ public class MainActivity extends Activity {
     super.onResume();
     try {
       URI masterUri = new URI("http://192.168.43.171:11311");
-      NodeConfiguration nodeConfiguration =
-//    	  NodeConfiguration.newPublic(InetAddressFactory.newNonLoopback().getHostName(), masterUri);
-	  NodeConfiguration.newPublic("192.168.43.1", masterUri);
-//      nodeConfiguration.setMasterUri(new URI("http://192.168.1.227:11311"));
-      tfTree = new TransformTree();
-      nodeRunner.run(tfPrinter, nodeConfiguration);
-//      Node node = new DefaultNodeFactory().newNode("android/tfl", nodeConfiguration);
-//      tfl = new TransformListener(node);
+      //tfTree = new TransformTree();
+      nodeRunner.run(tfPrinter, NodeConfiguration.newPublic("192.168.43.1", masterUri));
       oPubTf = new OrientationPublisherTf((SensorManager) getSystemService(SENSOR_SERVICE));
-      nodeRunner.run(oPubTf, nodeConfiguration);
+      nodeRunner.run(oPubTf, NodeConfiguration.newPublic("192.168.43.1", masterUri));
+      //Node node = new DefaultNodeFactory().newNode("android/tfl", NodeConfiguration.newPublic("192.168.43.1", masterUri));
+      //TransformListener tfl = new TransformListener(node);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
